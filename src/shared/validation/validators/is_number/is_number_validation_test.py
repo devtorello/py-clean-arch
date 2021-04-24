4c72597d52a2fe86d68308f10b8025cfc13a471b
Tatiana Vitorello <tatiana.vitorello@gentrop.com>
2021-04-24 14:44:18 -0300

    test: should throws if field value is not a number

--- src/shared/validation/validators/is_number/is_number_validation_test.py
+++ src/shared/validation/validators/is_number/is_number_validation_test.py
@@ -1,3 +1,5 @@
+import pytest
+from src.shared.validation.errors.is_number_error import is_number_error
 from .is_number_validation import IsNumberValidation
 
 FIELD = "valid_field"
@@ -11,3 +13,14 @@ def test_is_number_validation_valid():
     error = sut.validate({FIELD: 1})
 
     assert error is None
+
+
+def test_required_field_validation_invalid():
+    """ Should throws if field value is not a number """
+
+    sut = IsNumberValidation(FIELD)
+
+    with pytest.raises(TypeError) as execinfo:
+        sut.validate({FIELD: "1"})
+
+        assert execinfo == is_number_error(FIELD)
