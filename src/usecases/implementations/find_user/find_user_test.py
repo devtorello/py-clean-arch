b2ac4b90c2138f04ec8092757a0032daf91eb44e
Tatiana Vitorello <tatiana.vitorello@gentrop.com>
2021-04-24 13:54:20 -0300

    test: should return None if user does not exists

--- src/usecases/implementations/find_user/find_user_test.py
+++ src/usecases/implementations/find_user/find_user_test.py
@@ -1,4 +1,7 @@
+import pytest
 from faker import Faker
+from pytest_mock.plugin import MockerFixture
+from sqlalchemy.orm.exc import NoResultFound
 from src.entities.user import User
 from src.usecases.mocks import UserRepositoryStub
 from .find_user import FindUserUseCase
@@ -30,3 +33,23 @@ def test_find_user():
 
     assert result is not None
     assert isinstance(result, User)
+
+
+def test_find_user_not_fond(mocker: MockerFixture):
+    """ Should return None if user does not exists """
+
+    sut_variables = make_sut()
+
+    sut, user_repo = sut_variables["sut"], sut_variables["user_repo"]
+
+    user_id = faker.random_number()
+
+    with pytest.raises(NoResultFound):
+        mocker.patch.context_manager(
+            user_repo, "find", return_value=None, side_effect=NoResultFound()
+        )
+        result = sut.execute(user_id)
+
+        assert user_repo.find_user_params["id"] == user_id
+
+        assert result is None
