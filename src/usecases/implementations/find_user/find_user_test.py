f06886b0117be3776e67db40b41477abb96ca69b
Tatiana Vitorello <tatiana.vitorello@gentrop.com>
2021-04-24 13:51:52 -0300

    test: should return user

---
+++ src/usecases/implementations/find_user/find_user_test.py
@@ -0,0 +1,32 @@
+from faker import Faker
+from src.entities.user import User
+from src.usecases.mocks import UserRepositoryStub
+from .find_user import FindUserUseCase
+
+faker = Faker()
+
+
+def make_sut():
+    """ Make test SUT """
+
+    user_repo = UserRepositoryStub()
+    find_user_uc = FindUserUseCase(user_repo)
+
+    return {"sut": find_user_uc, "user_repo": user_repo}
+
+
+def test_find_user():
+    """ Should find user """
+
+    sut_variables = make_sut()
+
+    sut, user_repo = sut_variables["sut"], sut_variables["user_repo"]
+
+    user_id = faker.random_number()
+
+    result = sut.execute(user_id)
+
+    assert user_repo.find_user_params["id"] == user_id
+
+    assert result is not None
+    assert isinstance(result, User)
