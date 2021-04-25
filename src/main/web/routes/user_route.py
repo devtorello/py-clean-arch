bc777d63896f62988975ccd8a62f04818fbc776d
Tatiana Vitorello <tatiana.vitorello@gentrop.com>
2021-04-25 16:19:17 -0300

    feat: create fetch users route

--- src/main/web/routes/user_route.py
+++ src/main/web/routes/user_route.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request
 from src.main.factories.user.register_user import register_user_factory
+from src.main.factories.user.fetch_users import fetch_users_factory
 from src.main.factories.user.find_user import find_user_factory
 from src.main.web.config.route_adapt import route_adapter
 
@@ -18,3 +19,10 @@ def find_user():
     """ find user route """
 
     return route_adapter(request=request, controller=find_user_factory())
+
+
+@user_routes_bp.route("/fetch", methods=["GET"])
+def fetch_users():
+    """ fetch users route """
+
+    return route_adapter(request=request, controller=fetch_users_factory())
