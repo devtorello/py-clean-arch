c0a0ebb8c21508635623549f7ae12acfa35535b4
Tatiana Vitorello <tatiana.vitorello@gentrop.com>
2021-04-25 21:10:40 -0300

    test: should return 0 if user was not removed or found

--- src/usecases/implementations/user/remove_user/remove_user_test.py
+++ src/usecases/implementations/user/remove_user/remove_user_test.py
@@ -1,4 +1,5 @@
 from faker import Faker
+from pytest_mock.plugin import MockerFixture
 from src.usecases.mocks import UserRepositoryStub
 from .remove_user import RemoveUserUseCase
 
@@ -27,3 +28,19 @@ def test_remove_user_success():
 
     assert user_repo.remove_user_params["user_id"] == user_id
     assert result["users_removed"] == 1
+
+
+def test_remove_user_failure(mocker: MockerFixture):
+    """ Should return 0 if user was not removed or found """
+
+    sut_variables = make_sut()
+
+    sut, user_repo = sut_variables["sut"], sut_variables["user_repo"]
+
+    mocker.patch.context_manager(user_repo, "remove", return_value=0)
+
+    user_id = faker.random_number()
+
+    result = sut.execute(user_id=user_id)
+
+    assert result["users_removed"] == 0
